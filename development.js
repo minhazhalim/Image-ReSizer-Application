15258c420bff9a32c95a04719f2daec2353d1d22
Minhaz Halim (Zim) <31536403+minhazhalim@users.noreply.github.com>
2022-10-26 00:30:30 +0600

    Add files via upload

---
+++ development.js
@@ -0,0 +1,40 @@
+const resizerFile = document.querySelector('.resizer__file');
+const resizerInputWidth = document.querySelector('.resizer__input--width');
+const resizerInputHeight = document.querySelector('.resizer__input--height');
+const resizerAspect = document.querySelector('.resizer__aspect');
+const resizerCanvas = document.querySelector('.resizer__canvas');
+const zim = resizerCanvas.getContext('2d');
+let activeImage;
+let originalWidthToHeightRatio;
+function resize(width,height){
+     resizerCanvas.width = Math.floor(width);
+     resizerCanvas.height = Math.floor(height);
+     resizerInputWidth.value = Math.floor(width);
+     resizerInputHeight.value = Math.floor(height);
+     zim.drawImage(activeImage,0,0,Math.floor(width),Math.floor(height));
+}
+function openImage(imageSource){
+     activeImage = new Image();
+     activeImage.addEventListener('load',() => {
+          originalWidthToHeightRatio = activeImage.width / activeImage.height;
+          resize(activeImage.width,activeImage.height);
+     });
+     activeImage.src = imageSource;
+}
+resizerFile.addEventListener('change',(event) => {
+     const fileReader = new FileReader();
+     fileReader.addEventListener('load',() => {
+          openImage(fileReader.result);
+     });
+     fileReader.readAsDataURL(event.target.files[0]);
+});
+resizerInputWidth.addEventListener('change',() => {
+     if(!activeImage) return;
+     const heightValue = resizerAspect.checked ? resizerInputWidth.value / originalWidthToHeightRatio : resizerInputHeight.value;
+     resize(resizerInputWidth.value,heightValue);
+});
+resizerInputHeight.addEventListener('change',() => {
+     if(!activeImage) return;
+     const widthValue = resizerAspect.checked ? resizerInputHeight.value * originalWidthToHeightRatio : resizerInputWidth.value;
+     resize(widthValue,resizerInputHeight.value);
+});
--- script.js
+++ script.js
@@ -1,40 +1,43 @@
-const resizerFile = document.querySelector('.resizer__file');
-const resizerInputWidth = document.querySelector('.resizer__input--width');
-const resizerInputHeight = document.querySelector('.resizer__input--height');
-const resizerAspect = document.querySelector('.resizer__aspect');
-const resizerCanvas = document.querySelector('.resizer__canvas');
-const zim = resizerCanvas.getContext('2d');
-let activeImage;
-let originalWidthToHeightRatio;
-function resize(width,height){
-     resizerCanvas.width = Math.floor(width);
-     resizerCanvas.height = Math.floor(height);
-     resizerInputWidth.value = Math.floor(width);
-     resizerInputHeight.value = Math.floor(height);
-     zim.drawImage(activeImage,0,0,Math.floor(width),Math.floor(height));
-}
-function openImage(imageSource){
-     activeImage = new Image();
-     activeImage.addEventListener('load',() => {
-          originalWidthToHeightRatio = activeImage.width / activeImage.height;
-          resize(activeImage.width,activeImage.height);
+const uploadBox = document.querySelector('.upload-box');
+const previewImage = uploadBox.querySelector('img');
+const input = uploadBox.querySelector('input');
+const widthInput = document.querySelector('.width input');
+const heightInput = document.querySelector('.height input');
+const ratioInput = document.querySelector('.ratio input');
+const qualityInput = document.querySelector('.quality input');
+const downloadButton = document.querySelector('.download-button');
+let ogImageRatio;
+const loadFile = (event) => {
+     const file = event.target.files[0];
+     if(!file) return;
+     previewImage.src = URL.createObjectURL(file);
+     previewImage.addEventListener('load',() => {
+          widthInput.value = previewImage.naturalWidth;
+          heightInput.value = previewImage.naturalHeight;
+          ogImageRatio = previewImage.naturalWidth / previewImage.naturalHeight;
+          document.querySelector('.wrapper').classList.add('active');
      });
-     activeImage.src = imageSource;
-}
-resizerFile.addEventListener('change',(event) => {
-     const fileReader = new FileReader();
-     fileReader.addEventListener('load',() => {
-          openImage(fileReader.result);
-     });
-     fileReader.readAsDataURL(event.target.files[0]);
+};
+widthInput.addEventListener('keyup',() => {
+     const height = ratioInput.checked ? widthInput.value / ogImageRatio : heightInput.value;
+     heightInput.value = Math.floor(height);
 });
-resizerInputWidth.addEventListener('change',() => {
-     if(!activeImage) return;
-     const heightValue = resizerAspect.checked ? resizerInputWidth.value / originalWidthToHeightRatio : resizerInputHeight.value;
-     resize(resizerInputWidth.value,heightValue);
+heightInput.addEventListener('keyup',() => {
+     const width = ratioInput.checked ? heightInput.value * ogImageRatio : widthInput.value;
+     widthInput.value = Math.floor(width);
 });
-resizerInputHeight.addEventListener('change',() => {
-     if(!activeImage) return;
-     const widthValue = resizerAspect.checked ? resizerInputHeight.value * originalWidthToHeightRatio : resizerInputWidth.value;
-     resize(widthValue,resizerInputHeight.value);
-});
+const resizeAndDownload = () => {
+     const canvas = document.createElement('canvas');
+     const a = document.createElement('a');
+     const zim = canvas.getContext('2d');
+     const imageQuality = qualityInput.checked ? 0.5 : 1.0;
+     canvas.width = widthInput.value;
+     canvas.height = heightInput.value;
+     zim.drawImage(previewImage,0,0,canvas.width,canvas.height);
+     a.href = canvas.toDataURL('image/jpeg',imageQuality);
+     a.download = new Date().getTime();
+     a.click();
+};
+downloadButton.addEventListener('click',resizeAndDownload);
+uploadBox.addEventListener('click',() => input.click());
+input.addEventListener('change',loadFile);
